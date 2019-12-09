7212548b68deee58f935c7b803a732de130ba26b
Daniel Bouffard <bouffard.daniel25@gmail.com>
2019-12-09 15:03:23 -0500

    Add emacs build script.

---
+++ build_emacs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Script to build the latest emacs. Note that this is geared specifically
+# towards building it on Ubuntu 18.04.
+
+# so emacs server doesn't crash when the display closes. For gtk use gtk2. For
+# athena use athena
+TOOLKIT=athena
+# For gtk use libgtk2.0-dev, for anthena use libxaw7-dev
+TOOLKIT_PACKAGE=libxaw7-dev
+VERSION=26.3
+SRC_ROOT=/src
+INSTALL_DEST=/opt/emacs-$VERSION
+
+# Filesystem setup
+mkdir $SRC_ROOT
+chmod 777 $SRC_ROOT
+
+# Get the latest emacs
+wget -P /tmp https://ftp.gnu.org/gnu/emacs/emacs-$VERSION.tar.gz
+tar -xvf /tmp/emacs-$VERSION.tar.gz -C $SRC_ROOT
+rm /tmp/emacs-$VERSION.tar.gz
+
+# Install dependencies
+apt-get install -y $TOOLKIT_PACKAGE libgnutls28-dev libncurses5-dev libxpm-dev libjpeg-dev libtiff5-dev libpng-dev libgif-dev libxml2-dev libxft-dev libfreetype6-dev
+
+# Build and install emacs
+cd $SRC_ROOT/emacs-$VERSION
+./configure --prefix=$INSTALL_DEST --with-x-toolkit=$TOOLKIT
+make -j $(nproc)
+make install
+
+for bin in emacs emacsclient; do
+    ln -sf $INSTALL_DEST/bin/$bin /usr/bin/$bin
+done
