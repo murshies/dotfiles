35f44907c11ded9d21cad1a41c65250fbc7a760f
murshies <>
2020-11-04 18:29:40 +0000

    Update dockerrun.sh to optionally take a VNC port

--- ansible/dockerrun.sh
+++ ansible/dockerrun.sh
@@ -2,10 +2,15 @@
 
 container_name="$1"
 vnc_passwd="$2"
+vnc_port="$3"
+
+if [ -z "$vnc_port" ]; then
+    vnc_port=5901
+fi
 
 if [ -z "$container_name" ] || [ -z "$vnc_passwd" ]; then
-    echo "Usage: $0 <container_name> <vnc_passwd>"
+    echo "Usage: $0 <container_name> <vnc_passwd> [<vnc_port>]"
     exit 1
 fi
 
-docker run -p 5901:5901 -e VNCPASSWD="$vnc_passwd" -v /var/run/docker.sock:/var/run/docker.sock -v $HOME:/root/host --name "$container_name" -d devenv
+docker run -p $vnc_port:5901 -e VNCPASSWD="$vnc_passwd" -v /var/run/docker.sock:/var/run/docker.sock -v $HOME:/root/host --name "$container_name" -d murshies/devenv:latest
