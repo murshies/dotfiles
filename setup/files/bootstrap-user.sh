374f802b40d66c4bf947311cc1e83b22094e3727
murshies <>
2022-09-22 00:20:02 -0400

    Run dind.sh inside of server-mode.sh instead of bootstrap-user.sh

--- setup/files/bootstrap-user.sh
+++ setup/files/bootstrap-user.sh
@@ -2,6 +2,5 @@
 
 sudo cp -r /etc/skel/. $HOME
 echo $(whoami):$(whoami) | sudo chpasswd
-dind.sh
 install-gopls.sh
 install-emacs-packages.sh
--- setup/files/server-mode.sh
+++ setup/files/server-mode.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 bootstrap-user.sh
+dind.sh
 sudo service ssh start
 
 sleep infinity
