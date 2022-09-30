a0c16771d32894cf7b4e40e061b48b4dd129e824
murshies <>
2022-09-30 00:46:04 -0400

    Update dind.sh to work for multiple users

--- setup/files/dind.sh
+++ setup/files/dind.sh
@@ -8,5 +8,7 @@ fi
 if [ $(stat -c %G /var/run/docker.sock) == "UNKNOWN" ]; then
     docker_gid=$(stat -c %g /var/run/docker.sock)
     sudo groupadd -g $docker_gid dind
-    sudo usermod -a -G dind $(whoami)
 fi
+
+docker_group=$(stat -c %G /var/run/docker.sock)
+sudo usermod -a -G $docker_group $(whoami)
