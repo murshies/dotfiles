9e1838b19a5369d05f124bf84e6e805de1beecc6
murshies <>
2023-02-04 23:02:11 -0500

    Allow for virtualenv reusing

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
-cleanup() {
-    echo "Cleaning up virtualenv $venv_name"
-    rm -r $venv_name
-    trap '' EXIT INT TERM
-}
-trap cleanup EXIT INT TERM
-
-venv_name=$(mktemp -d)
-echo "Creating virtualenv in $venv_name"
-python3 -m venv $venv_name
-. $venv_name/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
+venv_name=/tmp/setup-venv-$(printf $(sha256sum requirements.txt))
+if [ ! -d "$venv_name" ]; then
+    echo "Creating virtualenv in $venv_name"
+    python3 -m venv $venv_name
+    . $venv_name/bin/activate
+    pip install --upgrade pip
+    pip install -r requirements.txt
+else
+    echo "venv $venv_name already exists, reusing it"
+    . $venv_name/bin/activate
+fi
 export PYTHONPATH=$(dirname $(realpath "$0"))
 echo "PYTHONPATH is $PYTHONPATH"
 ./setup.py "$@"
