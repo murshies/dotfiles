9c07c9bba7562ee368a62a73057cc7e5b17ecc5e
murshies <>
2023-02-05 08:19:11 -0500

    Do venv cleanup inside setup.sh instead of in the dockerfile

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+cleanup() {
+    echo "Cleaning up virtualenv $venv_name"
+    rm -r $venv_name
+    trap '' EXIT INT TERM
+}
+if [ ! -z "$CLEANUP_SETUP_VENV" ]; then
+    trap cleanup EXIT INT TERM
+fi
+
 venv_name=/tmp/setup-venv-$(printf $(sha256sum requirements.txt))
 if [ ! -d "$venv_name" ]; then
     echo "Creating virtualenv in $venv_name"
@@ -11,6 +20,7 @@ else
     echo "venv $venv_name already exists, reusing it"
     . $venv_name/bin/activate
 fi
+
 export PYTHONPATH=$(dirname $(realpath "$0"))
 echo "PYTHONPATH is $PYTHONPATH"
 ./setup.py "$@"
