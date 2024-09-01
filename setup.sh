d45c944027d34eeaafcf52aab1e66399df9ad534
murshies <>
2024-09-01 00:06:34 -0400

    Use full script path for sudo scripts

--- setup.sh
+++ setup.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+SCRIPT_PATH="$(dirname $(realpath "$0"))"
+
 cleanup() {
     if [ ! -z "$switch_to_permissive_sudo" ]; then
-        ./user-sudo.sh
+        $SCRIPT_PATH/user-sudo.sh
     fi
     trap '' EXIT INT TERM
 }
 trap cleanup EXIT INT TERM
 
 if ! grep -F 'NOPASSWD: ALL' /etc/sudoers.d/$(whoami) > /dev/null 2>&1 && [[ "$@" != *"-l"* ]]; then
-    ./user-sudo-all.sh
+    $SCRIPT_PATH/user-sudo-all.sh
     switch_to_permissive_sudo=y
 fi
 
-export PYTHONPATH="$(dirname $(realpath "$0"))"
+export PYTHONPATH="$SCRIPT_PATH"
 export NEEDRESTART_MODE=a
 export DEBIAN_FRONTEND=noninteractive
 echo "PYTHONPATH is $PYTHONPATH"
