3ffb30a31928f9d94b5b4cabde34a20f6e61fd67
murshies <>
2025-01-12 22:52:57 -0500

    Allow user to run `docker ps` via passwordless `sudo`

--- user-sudo.sh
+++ user-sudo.sh
@@ -7,6 +7,6 @@ fi
 
 cat << EOF | sudo tee /etc/sudoers.d/${target_user}
 ${target_user}	ALL=(ALL) ALL
-${target_user}	ALL=(ALL) NOPASSWD:SETENV: /usr/bin/apt-get update, /usr/bin/apt-get upgrade*
+${target_user}	ALL=(ALL) NOPASSWD:SETENV: /usr/bin/apt-get update, /usr/bin/apt-get upgrade*, /usr/bin/docker ps*
 EOF
 
